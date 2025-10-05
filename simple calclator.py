d421f4f3c076ef079391a539b61188a805c857fe
Omaia-Ux <om.andoura94@gmail.com>
2025-10-05 18:44:11 +0200

    Add files via upload
    simple calculator

---
+++ simple calclator.py
@@ -0,0 +1,23 @@
+# Simple Calculator
+# Author: Omaia
+# Date: 2025-08-03
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid operator")
+
+
